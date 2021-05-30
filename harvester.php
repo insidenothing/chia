b22e84723fe1aeec6a76d40224f6f8e668691faa
Patrick McGuire <insidenothing@gmail.com>
2021-05-30 13:32:18 -0400

    Update harvester.php

--- harvester.php
+++ harvester.php
@@ -3,10 +3,11 @@ $file = '/home/dad/.chia/mainnet/log/debug.log';
 $handle = fopen($file, "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
-        // process the line read.
-      echo "$line /n ";
+        $pos = strpos($line, 'Total');
+        if ($pos !== false) {
+                 echo "$line ";
+        } 
     }
-
     fclose($handle);
 } else {
     // error opening the file.
