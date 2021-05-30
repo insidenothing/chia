bc6b68f3bd94aa71c177d960d26938440cbc863e
Patrick McGuire <insidenothing@gmail.com>
2021-05-30 13:45:17 -0400

    Update harvester.php

--- harvester.php
+++ harvester.php
@@ -23,7 +23,8 @@ function log_last($search,$title){
             $pos = strpos($line, $search);
             $pos2 = strpos($line, date('Y-m-d'));
             if ($pos !== false && $pos2 !== false) {
-                     $last = "$title: $line ";
+                     $parts = explode($search,$line);
+                     $last = "$title: $parts[1] ";
             } 
         }
         fclose($handle);
@@ -37,4 +38,4 @@ log_search('2 plots were eligible','**');
 log_search('3 plots were eligible','***');
 log_search('4 plots were eligible','***');
 log_search('Found 1 proofs.','!!!!');
-log_last('Total','CURRENT PLOTS');
+log_last('Total','CURRENT STATUS');
