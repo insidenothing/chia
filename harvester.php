98cb74abd2304229f8f6aebcc1d17b5db0b2a68a
Patrick McGuire <insidenothing@gmail.com>
2021-05-30 13:33:31 -0400

    Update harvester.php

--- harvester.php
+++ harvester.php
@@ -3,9 +3,17 @@ $file = '/home/dad/.chia/mainnet/log/debug.log';
 $handle = fopen($file, "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
-        $pos = strpos($line, 'Total');
+        $pos = strpos($line, '3 plots were eligible');
         if ($pos !== false) {
-                 echo "$line ";
+                 echo "*** $line ";
+        } 
+        $pos = strpos($line, '2 plots were eligible');
+        if ($pos !== false) {
+                 echo "** $line ";
+        } 
+        $pos = strpos($line, '1 plots were eligible');
+        if ($pos !== false) {
+                 echo "* $line ";
         } 
     }
     fclose($handle);
