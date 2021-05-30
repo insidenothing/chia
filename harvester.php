c3104710e6d0ab8ddcd452e774b323a97a33f228
Patrick McGuire <insidenothing@gmail.com>
2021-05-30 13:43:51 -0400

    Update harvester.php

--- harvester.php
+++ harvester.php
@@ -14,10 +14,27 @@ function log_search($search,$stars){
     } else {
         // error opening the file.
     } 
-
+}
+function log_last($search,$title){ 
+    $file = '/home/dad/.chia/mainnet/log/debug.log';
+    $handle = fopen($file, "r");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            $pos = strpos($line, $search);
+            $pos2 = strpos($line, date('Y-m-d'));
+            if ($pos !== false && $pos2 !== false) {
+                     $last = "$title: $line ";
+            } 
+        }
+        fclose($handle);
+    } else {
+        // error opening the file.
+    } 
+    echo $last;
 }
 log_search('1 plots were eligible','*');
 log_search('2 plots were eligible','**');
 log_search('3 plots were eligible','***');
 log_search('4 plots were eligible','***');
 log_search('Found 1 proofs.','!!!!');
+log_last('Total','CURRENT PLOTS');
