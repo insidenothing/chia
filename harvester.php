064d6c1f1cbca1f1a0ecf3ccaa64101e69020d27
Patrick McGuire <insidenothing@gmail.com>
2021-05-30 13:48:34 -0400

    Update harvester.php

--- harvester.php
+++ harvester.php
@@ -15,6 +15,24 @@ function log_search($search,$stars){
         // error opening the file.
     } 
 }
+function log_count($search,$stars){ 
+    $file = '/home/dad/.chia/mainnet/log/debug.log';
+    $i=0;
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            $pos = strpos($line, $search);
+            $pos2 = strpos($line, date('Y-m-d'));
+            if ($pos !== false && $pos2 !== false) {
+                     //echo "$stars $line ";
+                $i++;
+            } 
+        }
+        fclose($handle);
+    } else {
+        // error opening the file.
+    } 
+    echo "$stars $search $i times today";
+}
 function log_last($search,$title){ 
     $file = '/home/dad/.chia/mainnet/log/debug.log';
     $handle = fopen($file, "r");
@@ -34,9 +52,9 @@ function log_last($search,$title){
     } 
     echo $last;
 }
-log_search('1 plots were eligible','*');
-log_search('2 plots were eligible','**');
-log_search('3 plots were eligible','***');
-log_search('4 plots were eligible','***');
-log_search('Found 1 proofs.','!!!!');
+log_count('1 plots were eligible','*');
+log_count('2 plots were eligible','**');
+log_count('3 plots were eligible','***');
+log_count('4 plots were eligible','****');
+log_search('Found 1 proofs.','!!!!!!!!');
 log_last('Total','Last Plot Count');
