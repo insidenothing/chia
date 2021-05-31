674c531c248da7ce18603711f8cb00bdb4b195a0
Patrick McGuire <insidenothing@gmail.com>
2021-05-31 08:14:10 -0400

    network size

---
+++ netspace_windows.php
@@ -0,0 +1,63 @@
+<?PHP
+function uplink($os,$datetime,$hostname,$total_plots,$proofs,$x1,$x2,$x3,$x4,$active_ploting,$disk_temp_free,$disk_final_free){
+    $db = mysqli_connect('watchdog-cluster-1.cluster-cmhaak521ogd.us-east-1.rds.amazonaws.com','service_user','service_pass','core') or die(mysqli_error($db));
+    $url = 'https://www.bmorecoin.com/harvester_uplink.php';
+    $ch = curl_init($url);
+    $datetime = $db->real_escape_string($datetime);
+    $jsonData = array(
+        'os' => "Windows",
+        'datetime' => "$datetime",
+        'hostname' => "$hostname",
+        'total_plots' => "$total_plots",
+        'proofs' => "$proofs",
+        'x1' => "$x1",
+        'x2' => "$x2",
+        'x3' => "$x3",
+        'x4' => "$x4",
+        'active_ploting' => "$active_ploting",
+        'disk_temp_free' => "$disk_temp_free",
+        'disk_final_free' => "$disk_final_free"
+    );
+    $jsonDataEncoded = json_encode($jsonData);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+    $result = curl_exec($ch);   
+}
+
+function netspace(){ 
+    ob_start();
+    $i=0;
+  ob_start();
+    //echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('C:\Users\Patrick\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia.exe show -s', $retval);
+
+// Printing additional info
+//echo '
+//</pre>
+//<hr />Last line of the output: ' . $last_line . '
+//<hr />Return value: ' . $retval; 
+    $buffer = ob_get_clean();
+$break='
+';
+    $a = explode($break,$buffer);
+    
+    foreach($a as $k => $v){
+              $pos2 = strpos($v, 'EiB');
+          if ($pos2 !== false) {
+                echo "$v  \r\n";
+            
+            } 
+    }
+    
+
+    $buffer = ob_get_clean();
+    echo $buffer;
+    return $buffer;
+}
+
+netspace();
